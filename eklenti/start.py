4acaeb047b988779c4e594ac8a20f41cfd60d388
seyidliadem <71027914+seyidliadem@users.noreply.github.com>
2021-04-04 22:41:33 +0400

    Create start.py

---
+++ eklenti/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Musiqi Kanali", url="https://t.me/MusiqiEuphoria")],
+        [InlineKeyboardButton(
+            "Elaqe 😊", url="https://t.me/SeyidliAdem")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
